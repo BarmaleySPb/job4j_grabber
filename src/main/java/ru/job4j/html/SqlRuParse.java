febca703e14d5724ede94de13b8c051b52703130
Barmaley <e.g.zakharov@gmail.com>
2021-09-29 18:11:55 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,14 +7,18 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element parent = td.parent();
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(parent.child(5).text());
+        int numberPage = 1;
+        while (numberPage < 6) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + numberPage).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element parent = td.parent();
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(parent.child(5).text());
+            }
+            numberPage++;
         }
     }
 }
