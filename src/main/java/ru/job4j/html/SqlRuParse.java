0ddc0ae9399da058540a2103ae93a6683a39e6e9
Barmaley <e.g.zakharov@gmail.com>
2021-09-29 20:01:32 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,8 +4,25 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.Post;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
 
 public class SqlRuParse {
+
+    private static Post detail(String link) throws IOException {
+        Post post = new Post();
+        Document docPost = Jsoup.connect(link).get();
+        Elements docPostDescription = docPost.select(".msgBody");
+        post.setDescription(docPostDescription.get(0).parent().child(1).text());
+        Elements docPostCreated = docPost.select(".msgFooter");
+        String[] postCrated = docPostCreated.get(0).parent().child(0).text().split("\\[");
+        SqlRuDateTimeParser dateTimeParser = new SqlRuDateTimeParser();
+        post.setCreated(dateTimeParser.parse(postCrated[0]));
+        return post;
+    }
+
     public static void main(String[] args) throws Exception {
         int numberPage = 1;
         while (numberPage < 6) {
@@ -17,6 +34,7 @@ public class SqlRuParse {
                 System.out.println(href.attr("href"));
                 System.out.println(href.text());
                 System.out.println(parent.child(5).text());
+                detail(href.attr("href"));
             }
             numberPage++;
         }
